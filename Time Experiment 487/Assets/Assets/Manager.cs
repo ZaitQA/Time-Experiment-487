b7ec69cca4b9bf5d37f62f7f77ecb9c68665e327
nicccccccA <32276411+nicccccccA@users.noreply.github.com>
2018-02-09 19:22:31 +0100

    Résaux Conf

---
+++ Time Experiment 487/Assets/Assets/Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+		PhotonNetwork.offlineMode = false;
+	}
+
+
+	void Update()
+	{
+		
+	}
+
+	 void OnJoinedLobby()
+	{
+		Debug.Log("Joined Lobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnJoinedRoom()
+	{
+		Debug.Log("Joined room");
+		GameObject j = PhotonNetwork.Instantiate("Sphere", Vector3.up * 5, Quaternion.identity, 0);
+		j.GetComponent<PlayerController>().enabled = true;
+
+	}
+	
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Random join Failed");
+		PhotonNetwork.CreateRoom("Room");
+	}
+
+	
+	
+}
