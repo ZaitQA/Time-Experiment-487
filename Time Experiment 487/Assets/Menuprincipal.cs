2e975e938a2bb7bdb902bbcd40bb5c581fd4b912
J3remiez <jeremiezeitoun@hotmail.fr>
2018-02-09 15:43:46 +0100

    Add Menu
    Le premier menu

---
+++ Time Experiment 487/Assets/Menuprincipal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuprincipal : MonoBehaviour
+
+
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
