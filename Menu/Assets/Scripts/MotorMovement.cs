05b045b9afb15d36fdbe45a0e8fec287c6037c1d
J3remiez <jeremiezeitoun@hotmail.fr>
2018-02-12 22:32:05 +0100

    Menuuu
    ,

---
+++ Menu/Assets/Scripts/MotorMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MotorMovement : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	public void MoveToPoint (Vector3 point)
+	{
+		agent.SetDestination(point);
+	}
+}
---
+++ Menu/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	private Camera cam;
+	public LayerMask mask;
+	private MotorMovement motor;
+	public Text hp;
+	private int life = 100;
+	private int maxlife = 100;
+	public Slider hpslider;
+	public GameObject deadText;
+	
+	void Start ()
+	{
+		cam = Camera.main;
+		motor = GetComponent<MotorMovement>();
+		hp.text = life + " / " + maxlife;
+	}
+	
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay((Input.mousePosition));
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100, mask))
+			{
+				motor.MoveToPoint(hit.point);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			if(life - 15 > 0)
+				life -= 15;
+			else
+			{
+				life = 0;
+				deadText.SetActive(true);
+			}
+			
+		}
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			if(life + 20 < maxlife)
+				life += 20;
+			else
+			{
+				life = maxlife;
+			}
+			
+		}
+
+		hp.text = life + " / " + maxlife;
+		hpslider.value = (float)life / maxlife;
+		
+	}
+
+	
+}
