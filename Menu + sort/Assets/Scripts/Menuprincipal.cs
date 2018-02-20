f9e38a4a2880ac8de6d320ae2f4ec11e5d8d41c9
nicccccccA <32276411+nicccccccA@users.noreply.github.com>
2018-02-20 21:37:07 +0100

    Menu + sorts
    Utilisez cette version si vous voulez faire des changements sur la scene

---
+++ Menu + sort/Assets/Scripts/Menuprincipal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuprincipal : MonoBehaviour
+
+
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Niveau1E-3");
+    }
+    
+    public void LevelMenu()
+    {
+        SceneManager.LoadScene("level");
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
