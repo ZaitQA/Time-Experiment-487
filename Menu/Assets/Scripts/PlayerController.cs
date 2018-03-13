da6f96eedf60c7d2b4c2e6705d5da9361ff9e34b
J3remiez <jeremiezeitoun@hotmail.fr>
2018-03-13 17:43:04 +0100

    fgfhgg

--- Menu/Assets/Scripts/PlayerController.cs
+++ Menu/Assets/Scripts/PlayerController.cs
@@ -8,58 +8,92 @@ public class PlayerController : MonoBehaviour
 {
 	private Camera cam;
 	public LayerMask mask;
-	private MotorMovement motor;
+	private PlayerMotor motor;
 	public Text hp;
+	public Text cons;
 	private int life = 100;
 	private int maxlife = 100;
 	public Slider hpslider;
 	public GameObject deadText;
-	
-	void Start ()
+	public GameObject porte;
+	public string[] inventaire;
+
+	void Start()
 	{
 		cam = Camera.main;
-		motor = GetComponent<MotorMovement>();
+		motor = GetComponent<PlayerMotor>();
 		hp.text = life + " / " + maxlife;
-	}
-	
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown(0))
+		inventaire = new string[10];
+		for (int i = 0; i < 10; i++)
 		{
-			Ray ray = cam.ScreenPointToRay((Input.mousePosition));
-			RaycastHit hit;
+			inventaire[i] = "";
+		}
+	}
 
-			if (Physics.Raycast(ray, out hit, 100, mask))
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, mask))
 			{
 				motor.MoveToPoint(hit.point);
 			}
 		}
 		if (Input.GetKeyDown(KeyCode.L))
 		{
-			if(life - 15 > 0)
+			if (life - 15 > 0)
 				life -= 15;
 			else
 			{
 				life = 0;
 				deadText.SetActive(true);
 			}
-			
+
 		}
 		if (Input.GetKeyDown(KeyCode.M))
 		{
-			if(life + 20 < maxlife)
+			if (life + 20 < maxlife)
 				life += 20;
 			else
 			{
 				life = maxlife;
 			}
-			
+
 		}
 
 		hp.text = life + " / " + maxlife;
-		hpslider.value = (float)life / maxlife;
-		
+		hpslider.value = (float) life / maxlife;
+
 	}
 
-	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "key")
+		{
+			cons.text = "Appuyer sur F pour rammasser la clé " + other.name + ".";
+		}
+
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "key")
+		{
+			cons.text = "";
+		}
+
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "key" && Input.GetKeyDown(KeyCode.F))
+		{
+			inventaire[0] = other.name;
+			other.gameObject.SetActive(false);
+			cons.text = "Tu as ramassé la clé " + inventaire[0];
+			
+		}
+
+	}
 }
---
+++ Menu/Assets/Scripts/openDoor.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform door;
+
+	public float speed;
+	private float move;
+	public bool opening;
+	public bool closing;
+	public float maxOpenz;
+	public float maxOpenx;
+	public bool secure;
+	public string key;
+	private string[] inv;
+	private GameObject Player;
+	private bool needKey = false;
+	public bool cote;
+	
+	void Start ()
+	{
+		inv = new string[10];
+		Player = GameObject.Find("Player");
+		maxOpenz = door.position.z;
+		maxOpenx = door.position.x;
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate()
+	{
+		if (opening)
+		{
+			Debug.Log("is opening");
+			OpenDoor();
+		}
+		if(closing)
+			CloseDoor();
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		inv = Player.GetComponent<PlayerController>().inventaire;
+		if (other.tag == "Player" && tag == "Porte" && !secure || secure && SearchKey(inv, key))
+		{
+			Debug.Log("entree");
+			secure = false;
+			opening = true;
+			closing = false;
+		}
+		else if(secure)
+		{
+			Player.GetComponent<PlayerController>().cons.text = "Tu as besoin de " + key;
+		}
+
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player" && tag == "Porte")
+		{
+			Debug.Log("Exit");
+			opening = false;
+			closing = true;
+			Player.GetComponent<PlayerController>().cons.text = "";
+		}
+	}
+
+	private bool SearchKey(string[] array, string Key)
+	{
+		for (int i = 0; i < array.Length; i++)
+		{
+			Debug.Log(array[i] + " " + Key);
+			if (array[i] == Key)
+				return true;
+		}
+		return false;
+	}
+
+	private void OpenDoor()
+	{
+		if (!cote)
+		{
+			if (door.position.z <= maxOpenz + 4)
+			{
+
+				move = 0.5f * Time.deltaTime * speed;
+
+				door.position = new Vector3(
+					door.position.x,
+					door.position.y,
+					door.position.z + move
+				);
+			}
+			else opening = false;
+		}
+		else if (cote)
+		{
+			if (door.position.x <= maxOpenx + 4)
+			{
+
+				move = 0.5f * Time.deltaTime * speed;
+
+				door.position = new Vector3(
+					door.position.x + move,
+					door.position.y,
+					door.position.z
+				);
+			}
+			else opening = false;
+		}
+}
+	private void CloseDoor()
+	{
+		if(!cote)
+		{
+			if (door.position.z >= maxOpenz)
+		{
+			move = 0.5f * Time.deltaTime * speed;
+
+			door.position = new Vector3(
+				door.position.x,
+				door.position.y,
+				door.position.z - move
+			);
+		}
+		else closing = false;
+			
+		}
+		else if (cote)
+		{
+			if (door.position.x >= maxOpenx)
+			{
+
+				move = 0.5f * Time.deltaTime * speed;
+
+				door.position = new Vector3(
+					door.position.x - move,
+					door.position.y,
+					door.position.z
+				);
+			}
+			else opening = false;
+		}
+	}
+	
+}
