0138bad04e15dc6b6ba60b44a410f4cc1228ca05
Pepit0Mc <32276187+Pepit0Mc@users.noreply.github.com>
2018-04-25 17:13:54 +0200

    ia

---
+++ Time-Experiment-the good one/Menu/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+    
+    BoxCollider boxCollider;            // Reference to the capsule collider.
+    bool isDead;                                // Whether the enemy is dead.
+    bool isSinking;                             // Whether the enemy has started sinking through the floor.
+
+
+    void Sart()
+    {
+        // Setting up the references.
+        boxCollider = GetComponent <BoxCollider> ();
+
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
+    }
+
+    void Update ()
+    {
+        // If the enemy should be sinking...
+        if(isSinking)
+        {
+            // ... move the enemy down by the sinkSpeed per second.
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        // If the enemy is dead...
+        if(isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // If the current health is less than or equal to zero...
+        if(currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        boxCollider.isTrigger = true;
+    }
+
+
+    public void StartSinking ()
+    {
+        // Find and disable the Nav Mesh Agent.
+        GetComponent <NavMeshAgent> ().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+        GetComponent <Rigidbody> ().isKinematic = true;
+
+        // The enemy should no sink.
+        isSinking = true;
+
+        // After 2 seconds destory the enemy.
+        Destroy (gameObject, 2f);
+    }
+}
---
+++ Time-Experiment-the good one/Menu/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+	private Transform player;               // Reference to the player's position.
+	//PlayerHealth playerHealth;      // Reference to the player's health.
+	//EnemyHealth enemyHealth;        // Reference to this enemy's health.
+	private NavMeshAgent agent;               // Reference to the nav mesh agent.
+
+
+	void Start()
+	{
+		// Set up the references.
+
+		//playerHealth = player.GetComponent <PlayerHealth> ();
+		//enemyHealth = GetComponent <EnemyHealth> ();
+	}
+
+
+	void Update()
+	{
+		// If the enemy and the player have health left...
+		agent = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		if(true //enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0
+			)
+		{
+			// ... set the destination of the nav mesh agent to the player.
+			float distance = Vector3.Distance(transform.position, player.position);
+			if(distance < 30)
+				agent.SetDestination (player.position);
+		}
+		// Otherwise...
+		//else
+		{
+			// ... disable the nav mesh agent.
+			//agent.enabled = false;
+		}
+	} 
+}
