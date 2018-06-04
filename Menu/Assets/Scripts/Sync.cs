8dfc1ff18b7fd7490d1af8cbc4b324f2956018d9
Pepit0Mc <32276187+Pepit0Mc@users.noreply.github.com>
2018-06-04 14:47:08 +0200

    sound and sync

---
+++ Menu/Assets/Scripts/Sync.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sync : Photon.MonoBehaviour
+{
+
+	private Vector3 networkPosition;
+	private Quaternion networkRotation;
+	private PhotonView photonView;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		photonView = GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!photonView.isMine)
+		{
+			transform.position = Vector3.Lerp(transform.position, networkPosition, Time.fixedDeltaTime * 20);
+			transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.fixedDeltaTime * 20);
+		}
+	}
+	
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}
+		else
+		{
+			networkPosition = (Vector3) stream.ReceiveNext();
+			networkRotation = (Quaternion) stream.ReceiveNext();
+		}
+	}
+}
